6569d83699c4f64a3b7036eddbe394d155396719
emegua <emeguag@gmail.com>
2021-04-20 08:57:16 +0900

    Now the server enables chat between multiple clients. Synchronization between sending and recving messages should be fixed.

--- chat/client.py
+++ chat/client.py
@@ -2,6 +2,7 @@ import os
 
 import asyncio
 import aiohttp
+import time
 
 HOST = os.getenv('HOST', '0.0.0.0')
 PORT = int(os.getenv('PORT', "8080"))
@@ -11,23 +12,59 @@ async def main():
     session = aiohttp.ClientSession()
     # To connect to a websocket server
     async with session.ws_connect(URL) as ws:
-        await p_send (ws)
+        error, user_id = await init_chat (ws)
+        if error:
+            return
+        await asyncio.gather(recv_msg(ws, user_id), send_msg(ws))
 
-        while True:
-            msg = await ws.receive()
-            print('Message received from server:', msg.data)
-            await p_send(ws)
-            if (msg.type) in (aiohttp.WSMsgType.CLOSE,
-                              aiohttp.WSMsgType.ERROR):
-                break
-
-
-async def p_send(ws):
-    msg = input('Type a message to send to the server: ')
-    if msg == 'exit':
+async def init_chat(ws):
+    user_id = input("Welcome to our group, if you want to continue type "
+          "your id, otherwise type exit.\n")
+    if user_id == 'exit':
         print('Exiting')
         raise SystemExit(0)
-    await ws.send_str(msg)
+
+    await ws.send_str(user_id)
+    msg = await ws.receive()
+    print(msg.data)     # FIXME what if the server wants to close the connection
+    error = 0
+    if (msg.type) in (aiohttp.WSMsgType.CLOSE,
+                      aiohttp.WSMsgType.ERROR):
+        error = 1
+    return (error, user_id)
+
+
+async def send_msg(ws):
+    while True:
+        await asyncio.sleep(0.01)
+        msg = input('Type a message to send to the server: ')
+        if msg == 'exit':
+            print('Exiting')
+            # raise SystemExit(0)
+        await ws.send_str(msg)
+
+
+
+
+async def recv_msg (ws, user_id):
+    while True:
+        #print('waiting')
+        msg = await ws.receive()
+        print(msg.data)
+        """if msg.data.user == user_id:
+            pass
+        elif msg.data.user == "server":
+            #print(msg.data)
+            print('%s:  %s' % (msg.data.user, msg.data.msg)) # FIXME server msg should be alligned at the center
+        else:
+            #print(msg.data)
+            print('%s:  %s' % (msg.data.user, msg.data.msg)) # logging should be improved
+"""
+        if (msg.type) in (aiohttp.WSMsgType.CLOSE,
+                          aiohttp.WSMsgType.ERROR):
+            raise SystemExit(0)
+            break
+        await asyncio.sleep(0.01)
 
 if __name__ == '__main__':
     print('Type "exit" to quit')
