1c158f413adc7dfd3a575314567c5842a2a16344
emegua <emeguag@gmail.com>
2021-04-20 02:28:56 +0900

    Simple web server implemented.

---
+++ chat/settings.py
@@ -0,0 +1,13 @@
+import pathlib
+import yaml
+
+# BASE_DIR = pathlib.Path(__file__)
+BASE_DIR = pathlib.Path(__file__).parent.parent
+# print(BASE_DIR)
+
+# def get_config (path):
+#    with open(path) as config_file:
+#        config = yaml.safe_load(config_file)
+#    return config
+
+# config = get_config(config_path)
